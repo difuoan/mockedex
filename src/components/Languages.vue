6ad2fb829487a0bf0a9770b0f5137618b1623883
difu0an <lucas.j.venturini@gmail.com>
2021-01-30 10:39:13 +0100

    made a component to select the language

---
+++ src/components/Languages.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="languages">
+    <select
+      name="languages"
+      v-model="$store.state.language"
+      class="p-1 rounded d-inline-block"
+    >
+      <option
+        v-for="language in languages"
+        :value="language.index"
+        :key="`language_${language.id}`"
+        >{{ language.name }}</option
+      >
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import languages from "../mixins/languages";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Languages",
+  mixins: [languages]
+});
+</script>
