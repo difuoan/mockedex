534a596a95ed4906bc7510cd1ec18a29aa9c5263
difu0an <lucas.j.venturini@gmail.com>
2021-02-04 20:50:41 +0100

    finally found out how to mock axios

--- tests/unit/name.spec.ts
+++ tests/unit/name.spec.ts
@@ -1,37 +1,44 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, flushPromises } from "@vue/test-utils";
 import Name from "@/components/Name.vue";
+import { createStore } from "vuex";
+import methods from "@/mixins/methods";
 
 describe("Name.vue", () => {
-  it("loads the pokémons name and displays it int the correct language", async () => {
-    const name = "test 1";
-    const $axios = jest.mock("axios", () => ({
-      get: Promise.resolve({
-        data: {
-          names: [
-            {
-              language: {
-                name: "de",
-                url: "testUrl"
-              },
-              name: "test 2 de"
-            },
-            {
-              language: {
-                name: "en",
-                url: "testUrl"
-              },
-              name: "test 2 en"
+  const name = "test 1";
+  const store = createStore({
+    state: {
+      apiUrl: "test 4",
+      language: "test 3"
+    }
+  });
+  const axios = {
+    get: async () => ({
+      data: {
+        names: [
+          {
+            name: "test 2",
+            language: {
+              name: "test 3",
+              url: ""
             }
-          ]
-        }
-      })
-    }));
+          }
+        ]
+      }
+    })
+  };
+  it("loads the pokémons name and displays it in the correct language", async () => {
     const wrapper = shallowMount(Name, {
       props: { name },
-      mocks: { $axios }
+      global: {
+        plugins: [store],
+        mixins: [methods],
+        mocks: {
+          axios: axios
+        }
+      }
     } as any);
     expect(wrapper.html()).toContain("test 1");
-    await new Promise(resolve => setTimeout(resolve, 10));
+    await flushPromises();
     expect(wrapper.html()).toContain("test 2");
   });
 });
