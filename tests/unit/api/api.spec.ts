6adb4e84e5b296bb2a345286a562d6596dac6333
difu0an <lucas.j.venturini@gmail.com>
2021-02-06 07:29:38 +0100

    added a new unit test that tests the status of the api because we will need the api to be currently available to actually run the app

---
+++ tests/unit/api/api.spec.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+describe("Name.vue", () => {
+  // TEST 1 - START //////////////////////////////////////////////////////////////////////////////////////////
+  // use await so jest doesn't get confused when chaining the tests
+  it(`simply test the status of the v2 api because we need it
+      (we don't test the actual data-structure because it should stay the same within v2)`, async () => {
+    const axiosSpy = jest.spyOn(axios, "get");
+    await axios.get("https://pokeapi.co/api/v2/pokemon/1").then(response => {
+      expect(response.status).toStrictEqual(200);
+    });
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    await axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=1&offset=0")
+      .then(response => {
+        expect(response.status).toStrictEqual(200);
+      });
+    expect(axiosSpy).toHaveBeenCalledTimes(2);
+  });
+  // TEST 1 - END //////////////////////////////////////////////////////////////////////////////////////////
+});
